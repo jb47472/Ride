9c1282f73b5e16c6d93b8ebee2c07386b3033279
benzed3 <benzed3@gmail.com>
2019-06-11 19:02:02 -0400

    pages built with css

--- ride/src/App.js
+++ ride/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Search from "./components/pages/Search";
+import Options from "./components/pages/Options";
+import Result from "./components/pages/Result";
+import NavTabs from "./components/NavTabs";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <Wrapper>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/options" component={Options} />
+          <Route exact path="/result" component={Result} />
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
 
