eab270d723997b32e1ef8994ff0572798f7cab6f
benzed3 <benzed3@gmail.com>
2019-06-29 11:08:30 -0400

    Updates

---
+++ precache-manifest.8c52f582f4df9d17ac2b8cf89d66d2ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2527ba8da58753acd347e61f92e692",
+    "url": "/Ride/index.html"
+  },
+  {
+    "revision": "8171b0ff49ccd8c1671a",
+    "url": "/Ride/static/css/main.eea4a28f.chunk.css"
+  },
+  {
+    "revision": "a52100f782d94189b311",
+    "url": "/Ride/static/js/2.08815305.chunk.js"
+  },
+  {
+    "revision": "8171b0ff49ccd8c1671a",
+    "url": "/Ride/static/js/main.ff6a2498.chunk.js"
+  },
+  {
+    "revision": "95f7bd2721ce091e6318",
+    "url": "/Ride/static/js/runtime~main.0b3f8814.js"
+  },
+  {
+    "revision": "ac23c038f3f6d60c35e2c63cd1b972de",
+    "url": "/Ride/static/media/car2.ac23c038.png"
+  }
+]);
